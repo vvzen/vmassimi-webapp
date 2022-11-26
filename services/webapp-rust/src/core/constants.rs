6803ba9b96d0f8c9d53af48beb2b12af3db13a71
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-11-26 16:20:12 +0000

    refactor: Structure Rust code in different modules

---
+++ services/webapp-rust/src/core/constants.rs
@@ -0,0 +1,9 @@
+pub const PORT_NUM: u16 = 3000;
+pub const APP_VERSION: &'static str = "v0.1.0";
+pub const ENTRY_POINT_DIR_NAME: &'static str = "programm"; // arbitrary name given by vmassimi
+
+pub const ARCHIVES_ROOT_DIR: &'static str = "/app/data/archives";
+pub const ARCHIVES_TMP_DIR: &'static str = "/app/data/archives/tmp";
+pub const VERSIONS_PATH: &'static str = "/app/data/versions.json";
+
+pub const ZFILL_PADDING: usize = 3;
