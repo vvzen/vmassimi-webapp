07c9274ab21f1fc8fb766efe255cd77cbc19354b
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2022-12-20 00:11:44 +0000

    fix: Bash script for generating a single image

--- services/webapp-rust/scripts/test_recipe_algo.sh
+++ services/webapp-rust/scripts/test_recipe_algo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -e
+
 input_dir=/Users/valerioviperino/dev/personal/vmassimi/sample-files/sphynx_program/programm
-
-recipe=$(python3 generate_permutation.py $input_dir)
-
 image_app=~/dev/personal/vmassimi/vmassimi-tools/scripts/image-composite/image-composite-macos
-recipe_checksum=$(echo $recipe | sha256sum | cut -c 8)
+
+#recipe=$(python3 generate_permutation.py $input_dir)
+python3 generate_permutation.py $input_dir 1>tmp_recipe 2>tmp_recipe_stderr
+recipe_checksum=$(cat tmp_recipe | md5sum | awk '{print $1}' | cut -c-8)
 image_name="image_$recipe_checksum"
 
-echo $recipe | $image_app --image-name $image_name
+echo $image_name
+
+cat tmp_recipe | $image_app --image-name $image_name
